a6bf84a317d0bd26beaef6b4db7a2e60cbddf02b
michal-php <72819625+michal-php@users.noreply.github.com>
2020-10-13 20:42:35 +0200

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,12 @@
+<?php
+
+class Config{
+
+    const DOMAIN = '';
+    // Routing defaults
+    const DEFAULT_MODULE = 'Dns';
+    const DEFAULT_ACTION = 'listAction';
+    // REST API
+    const  API_KEY = '';
+    const  SECRET = '';
+}
